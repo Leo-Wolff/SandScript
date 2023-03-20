f7b9572fc9da64506a5d92a2ed30a7f54427a2a2
Kboere <kevin.boere@hva.nl>
2023-03-20 16:03:06 +0100

    basic ejs files to copy

--- server.js
+++ server.js
@@ -2,8 +2,15 @@ const express = require('express')
 const app = express()
 const port = 8000
 
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+app.get('/filter', (req, res) => {
+  res.render('pages/filter')
+})
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello Worldgit stat!')
 })
 
 app.listen(port, () => {
