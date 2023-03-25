4c02f1beaa7a1fa3344da1f70f10050af24e9d35
Robin500818167 <robin.van.houten@hva.nl>
2023-03-25 11:51:45 +0100

    add engine

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 const port = 8000
 
+// set the view engine to ejs
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, '/static')))
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
