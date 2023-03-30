e71f52f7674349347798bb78b31b3568fe115e27
Leo-Wolff <leokramer17@gmail.com>
2023-03-30 11:39:41 +0200

    Update MONGODB_URI variable name

--- database/connection.js
+++ database/connection.js
@@ -1,18 +1,16 @@
-const mongoose = require('mongoose')
-
+const mongoose = require("mongoose")
 
 const connection = () => {
-    try {
-        mongoose.connect(process.env.URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log('Database connection is established!');
-    } catch (err) {
-        console.log('Error while connecting database: ', err);
-        throw err;
-    }
-
+	try {
+		mongoose.connect(process.env.MONGODB_URI, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		})
+		console.log("Database connection is established!")
+	} catch (err) {
+		console.log("Error while connecting database: ", err)
+		throw err
+	}
 }
 
-module.exports = connection;
+module.exports = connection
