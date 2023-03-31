a7a08eb4226760f724d8f7634e78bc19903f1156
svendoejaaren <svendoejaaren@hotmail.com>
2023-03-31 12:15:28 +0200

    Register account works

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-require("dotenv").config()
-const express = require("express")
+require('dotenv').config()
+const express = require('express')
 const app = express()
 const port = 8000
-const path = require("path")
-const cookieParser = require("cookie-parser")
-const session = require("express-session")
-const bodyParser = require("body-parser")
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const bodyParser = require('body-parser')
 
 // Connecting mongoDB
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb")
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -18,76 +18,76 @@ app.use(bodyParser.json())
 
 const uri = process.env.MONGODB_URI
 const client = new MongoClient(uri, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-	serverApi: ServerApiVersion.v1,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 })
 
-const dbName = "sandscript"
+const dbName = 'sandscript'
 
 // collections aanroepen
 global.db = client.db(dbName)
-global.users = db.collection("users")
+global.users = db.collection('users')
 
 async function connectToDatabase() {
-	try {
-		console.log("Connecting to MongoDB Atlas cluster...")
-		await client.connect()
-		console.log("Successfully connected to MongoDB Atlas!")
-		return client
-	} catch (error) {
-		console.error("Connection to MongoDB Atlas failed!", error)
-		process.exit()
-	}
+  try {
+    console.log('Connecting to MongoDB Atlas cluster...')
+    await client.connect()
+    console.log('Successfully connected to MongoDB Atlas!')
+    return client
+  } catch (error) {
+    console.error('Connection to MongoDB Atlas failed!', error)
+    process.exit()
+  }
 }
 
 connectToDatabase()
 
 async function CreateNewDraft(collection, content, input) {
-	const draft = {
-		text: content,
-		signed: input,
-		dateUpdated: new Date().toISOString().slice(0, 10), // oorspronkelijk date is handiger voor aanpassen later
-	}
+  const draft = {
+    text: content,
+    signed: input,
+    dateUpdated: new Date().toISOString().slice(0, 10), // oorspronkelijk date is handiger voor aanpassen later
+  }
 
-	await collection.insertOne(draft)
+  await collection.insertOne(draft)
 }
 
 // set the view engine to ejs
-app.set("view engine", "ejs")
+app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true }))
 
-app.use(express.static(path.join(__dirname, "/static")))
+app.use(express.static(path.join(__dirname, '/static')))
 app.use(cookieParser())
 
 //Session
-app.set("trust proxy", 1)
+app.set('trust proxy', 1)
 app.use(
-	session({
-		secret: "keyboard cat",
-		resave: false,
-		saveUninitialized: true,
-		cookie: {},
-	})
+  session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {},
+  })
 )
 //Login page
-app.post("/uitloggen", (req, res) => {
-	req.session.destroy()
-	res.redirect("/inloggen")
+app.post('/uitloggen', (req, res) => {
+  req.session.destroy()
+  res.redirect('/inloggen')
 })
-app.get("/inloggen", (req, res) => {
-	res.render("pages/inloggen")
+app.get('/inloggen', (req, res) => {
+  res.render('pages/inloggen')
 })
-app.post("/inloggen", async (req, res) => {
-	const currentUser = await users.findOne({
-		username: req.body.username,
-	})
-	req.session.user = {
-		username: currentUser.username,
-		password: currentUser.password,
-		email: currentUser.email,
-	}
-	res.redirect("/account")
+app.post('/inloggen', async (req, res) => {
+  const currentUser = await users.findOne({
+    username: req.body.username,
+  })
+  req.session.user = {
+    username: currentUser.username,
+    password: currentUser.password,
+    email: currentUser.email,
+  }
+  res.redirect('/account')
 })
 
 //Profile page
@@ -99,34 +99,34 @@ app.post("/inloggen", async (req, res) => {
 // 	})
 // })
 
-app.post("/update", async (req, res) => {
-	await users.findOneAndUpdate(
-		{
-			username: req.session.user.username,
-		},
-		{
-			$set: {
-				username: req.body.username,
-				email: req.body.email,
-			},
-		}
-	)
-	req.session.user.username = req.body.username
-	req.session.user.email = req.body.email
-	res.redirect("/account")
+app.post('/update', async (req, res) => {
+  await users.findOneAndUpdate(
+    {
+      username: req.session.user.username,
+    },
+    {
+      $set: {
+        username: req.body.username,
+        email: req.body.email,
+      },
+    }
+  )
+  req.session.user.username = req.body.username
+  req.session.user.email = req.body.email
+  res.redirect('/account')
 })
 
-const editorRoutes = require("./routes/editor.js")
-app.use("/editor", editorRoutes)
+const editorRoutes = require('./routes/editor.js')
+app.use('/editor', editorRoutes)
 
-const homeRoutes = require("./routes/home.js")
-app.use("/home", homeRoutes)
+const homeRoutes = require('./routes/home.js')
+app.use('/home', homeRoutes)
 
-app.post("/bottle", (req, res) => {
-	const db = client.db(dbName)
-	const collectionLetters = db.collection("letters")
-	CreateNewDraft(collectionLetters, req.body.content, req.body.signed)
-	res.render("pages/bottle")
+app.post('/bottle', (req, res) => {
+  const db = client.db(dbName)
+  const collectionLetters = db.collection('letters')
+  CreateNewDraft(collectionLetters, req.body.content, req.body.signed)
+  res.render('pages/bottle')
 })
 
 // discover page
@@ -134,7 +134,7 @@ app.post("/bottle", (req, res) => {
 // 	try {
 // 		const filters = req.cookies.selectedFilters
 // 			? JSON.parse(req.cookies.selectedFilters)
-// 			: {} // get filters from cookie 
+// 			: {} // get filters from cookie
 
 // 			const ik = await users.findOne({username: 'MysteryMan'})
 // 			const eersteMatch = await users.findOne({...filters, username: { $nin: ik.likes, $not: {$eq: ik.username} }, status: 'new'})
@@ -167,60 +167,88 @@ app.post("/bottle", (req, res) => {
 
 // filtering in discover page
 app.post('home/discover', async (req, res) => {
-	try {
-		const filters = { gender: req.body.gender } // save input from user in filters
+  try {
+    const filters = { gender: req.body.gender } // save input from user in filters
 
-		res.cookie("selectedFilters", JSON.stringify(filters)) // save filters in cookie
+    res.cookie('selectedFilters', JSON.stringify(filters)) // save filters in cookie
 
-		console.log(req.session);
+    console.log(req.session)
 
-		const ik = await users.findOne({username: 'MysteryMan'})
-		const eersteMatch = await users.findOne({...filters, username: { $nin: ik.likes, $not: {$eq: ik.username} }, status: 'new'})
+    const ik = await users.findOne({ username: 'MysteryMan' })
+    const eersteMatch = await users.findOne({
+      ...filters,
+      username: { $nin: ik.likes, $not: { $eq: ik.username } },
+      status: 'new',
+    })
 
-		if (eersteMatch) {
-			res.render("pages/gefiltered", { eersteMatch })
-		} else {
-			res.send("no results")
-		}
-	} catch (err) {
-		console.log(err.stack)
-	}
+    if (eersteMatch) {
+      res.render('pages/gefiltered', { eersteMatch })
+    } else {
+      res.send('no results')
+    }
+  } catch (err) {
+    console.log(err.stack)
+  }
 })
 
-app.post("/liked", async (req, res) => {
-	try {
-		const eersteMatch = await users.findOne({
-			_id: new ObjectId(req.body.matchId)
-		})
+app.post('/liked', async (req, res) => {
+  try {
+    const eersteMatch = await users.findOne({
+      _id: new ObjectId(req.body.matchId),
+    })
 
-		const ik = await users.findOne({username: 'MysteryMan'})
-		console.log(eersteMatch)
+    const ik = await users.findOne({ username: 'MysteryMan' })
+    console.log(eersteMatch)
 
-		await users.updateOne(
-			{ _id: ik._id },
-			{ $push: { likes: eersteMatch.username} }
-		)
+    await users.updateOne(
+      { _id: ik._id },
+      { $push: { likes: eersteMatch.username } }
+    )
 
-		await users.updateOne(
-			{ _id: eersteMatch._id },
-			{ $push: { likedBy: ik.username } }
-		)
+    await users.updateOne(
+      { _id: eersteMatch._id },
+      { $push: { likedBy: ik.username } }
+    )
 
-		ik.likes.push(eersteMatch.username)
-		eersteMatch.likedBy.push(ik.username)
+    ik.likes.push(eersteMatch.username)
+    eersteMatch.likedBy.push(ik.username)
 
-		if (ik.likes.includes(eersteMatch.username) && ik.likedBy.includes(eersteMatch.username)) {
-			console.log('match')
-			res.redirect('home/discover')
-		} else {
-			console.log('geen match')
-			res.redirect('home/discover')
-		}
-	} catch (err) {
-		console.log(err.stack)
-	}
+    if (
+      ik.likes.includes(eersteMatch.username) &&
+      ik.likedBy.includes(eersteMatch.username)
+    ) {
+      console.log('match')
+      res.redirect('home/discover')
+    } else {
+      console.log('geen match')
+      res.redirect('home/discover')
+    }
+  } catch (err) {
+    console.log(err.stack)
+  }
+})
+
+// Register
+app.get('/register', (req, res) => {
+  res.render('pages/register.ejs')
+})
+
+app.post('/account', (req, res) => {
+  const username = req.body.username
+  const email = req.body.email
+  const password = req.body.password
+
+  const user = {
+    username,
+    email,
+    password,
+  }
+
+  users.insertOne(user)
+
+  res.render('pages/account.ejs')
 })
 
 app.listen(port, () => {
-	console.log(`Wow! Look at that ${port}`)
+  console.log(`Wow! Look at that ${port}`)
 })
