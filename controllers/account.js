e4ff594879290f3989985c10b2b78a9461e67da3
Robin500818167 <robin.van.houten@hva.nl>
2023-04-01 15:35:33 +0200

    add and edit update account

--- controllers/account.js
+++ controllers/account.js
@@ -11,11 +11,16 @@ exports.inloggen1 = async (req, res) => {
 	const currentUser = await users.findOne({
 		username: req.body.username,
 	})
-	if(currentUser) {
+	if(currentUser) { 
 	req.session.user = {
 		username: currentUser.username,
 		password: currentUser.password,
 		email: currentUser.email,
+		name: currentUser.name,
+		age: currentUser.age,
+		gender: currentUser.gender,
+		interests: currentUser.interests,
+		language: currentUser.language,
 	}
 	res.redirect("/account")
 }
@@ -34,19 +39,40 @@ exports.update = async (req, res) => {
 			$set: {
 				username: req.body.username,
 				email: req.body.email,
+				password: req.body.password,
+				name: req.body.name,
+				age: req.body.age,
+				gender: req.body.gender,
+				interests: req.body.interests,
+				country: req.body.country,
+				language: req.body.language,
 			},
 		}
 	)
 	req.session.user.username = req.body.username
 	req.session.user.email = req.body.email
+	req.session.user.password = req.body.password
+	req.session.user.name = req.body.name
+	req.session.user.age = req.body.age
+	req.session.user.interests = req.body.interests
+	req.session.user.country = req.body.country
+	req.session.user.language = req.body.language
+	req.session.user.gender = req.body.gender
 	res.redirect("/account")
 }
 
 // Profile page
 exports.account = async (req, res) => {
-	const { username, email } = req.session.user
+	const { username, email, password, name, age, gender, interests, country, language } = req.session.user
 	res.render("pages/account", {
 		username: username,
-		email: email
+		email: email,
+		password: password,
+		name: name,
+		age: age,
+		gender: gender,
+		interests: interests,
+		country: country,
+		language: language
 	})
 }
